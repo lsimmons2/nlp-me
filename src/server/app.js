99dd0b9751a6898b342732b5661dee11452fe25a
Leo <leooscar.simmons@gmail.com>
2016-10-27 12:30:14 -0400

    organize, babel move non js files

--- src/server/app.js
+++ src/server/app.js
@@ -6,7 +6,7 @@ import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
-import routes from './server/routes';
+import routes from './routes';
 
 const app = express();
 
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/vendor', express.static(path.join(__dirname, '../node_modules')));
+app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/vendor', express.static(path.join(__dirname, '../../node_modules')));
 app.use('/nlp', routes);
 
 
