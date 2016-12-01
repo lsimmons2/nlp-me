201620a33abb4f4f250e9c6bdfc917021e2aa40e
Leo Simmons <leooscar.simmons@gmail.com>
2016-12-01 11:16:25 -0500

    feedback validation

--- src/public/controllers/aboutCtrl.js
+++ src/public/controllers/aboutCtrl.js
@@ -8,23 +8,25 @@ angular.module('aboutCtrl', [])
   $scope.feedbackError = false;
 
   $scope.feedback = function(){
-    var data = {
-      name: $scope.name,
-      email: $scope.email,
-      message: $scope.message
-    };
-    $http.post('/feedback', data)
-      .then(function(resp){
-        $scope.name = '';
-        $scope.email = '';
-        $scope.message = '';
-        $scope.feedbackError = false;
-        $scope.feedbackSuccess = true;
-      })
-      .catch(function(err){
-        $scope.feedbackSuccess = false;
-        $scope.feedbackError = true;
-      });
-  }
+    if ($scope.contactForm.$valid){
+      var data = {
+        name: $scope.name,
+        email: $scope.email,
+        message: $scope.message
+      };
+      $http.post('/feedback', data)
+        .then(function(resp){
+          $scope.name = '';
+          $scope.email = '';
+          $scope.message = '';
+          $scope.feedbackError = false;
+          $scope.feedbackSuccess = true;
+        })
+        .catch(function(err){
+          $scope.feedbackSuccess = false;
+          $scope.feedbackError = true;
+        });
+      }
+    }
 
 });
