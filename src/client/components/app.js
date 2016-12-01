1279c514c5e4d7a5671d17278802daac5ca6c898
Leo Simmons <leooscar.simmons@gmail.com>
2016-11-30 22:11:11 -0500

    routing

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 import ChatBox from './chat-box';
+import AboutPage from './about-page';
 import '../style/main.scss';
-class App extends React.Component {
 
+
+class App extends React.Component {
   render() {
     return (
-      <div>
-        < ChatBox />
-      </div>
+      <Router history={hashHistory}>
+        <Route path='/' component={AboutPage} />
+        <Route path='/about' component={AboutPage} />
+        <Route path='/chat' component={ChatBox} />
+      </Router>
     )
   }
-
 }
+
 
 export default App
